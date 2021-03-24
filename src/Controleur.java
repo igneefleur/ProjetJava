a700e7ca8c7b5d960c43e6b765e10a577f01dfe8
Edgar Poujol <epoujo01@pcalb-mm0000.univ-jfc.fr>
2021-03-24 12:17:32 +0100

    modif control + ajout pour menuselector

--- src/Controleur.java
+++ src/Controleur.java
@@ -1,24 +1,53 @@
-import java.awt.event.ItemEvent;
+import java.awt.event.ItemEvent; 
 import java.awt.event.ItemListener;
+import java.util.Arrays;
+
 
 public class Controleur implements ItemListener{
 	
 	private Modele mdl;
 	
 	public Controleur (Modele mdl) {
-		this.mdl =mdl;
-	}
-	
-	public void itemStateChanged(ItemEvent arg) {
-		System.out.println(arg.getItem());
-		Integer n = (Integer)arg.getItem();
-		this.mdl.selctImage();
-	}
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		this.mdl = mdl;
 	}
 
+	@Override
+	public void itemStateChanged(ItemEvent arg0) {
+		if (arg0.getSource()instanceof java.awt.List) {
+			
+			
+			
+		}
+		else if (arg0.getSource() instanceof java.awt.Checkbox) {
+			
+			
+		}
+			
+		}
+		
+	
+
+}
+Checkbox[] note = new Checkbox[5];
+
+String[] descrip = {"1 étoile", "2 étoiles","3 étoiles", "4 étoiles", "5 étoiles"};
+
+
+	CheckboxGroup CG = new CheckboxGroup();
+	ItemListener itemlistener = new ItemListener() {
+		@Override
+		public void itemStateChanged(ItemEvent arg0) {
+			
+			// TODO Auto-generated method stub
+		}
+	};
+	
+	
+	for (int i=0; i<5; i++){
+		note[i] = new Checkbox(descrip[i], false, CG);
+		note[i].addItemListener(itemlistener);
+		add(note[i]);
+	}
+	
+	Modele.addObserver(this);
 }
