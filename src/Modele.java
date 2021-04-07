0c447f4a12a3a13c5b8ca94d03f745a2b79de3d6
SDM0 <60699143+SDM0@users.noreply.github.com>
2021-04-07 12:17:21 +0200

    Ajout de recettes + facile

--- src/Modele.java
+++ src/Modele.java
@@ -83,12 +83,34 @@ public class Modele implements PropertyChangeListener {
 	
 	public static void main(String[] args) {
 		Modele m = new Modele();
-		Recette r1 = new Recette("Gateau",2,new String[]{"Sucré", "Moelleux"},"Utiliser des ingrédients");
-		Recette r2 = new Recette("Crepes",1,new String[]{"Sucré"},"Préparer une poele");
-		m.ajouterRecette(r1);
-		m.ajouterRecette(r2);
-		System.out.println(m.toString());
-		m.supprimerRecette(r1);
+		Recette r1 = new Recette("Moelleux au chocolat",2,new String[]{"Sucre", "Moelleux"},"Etape 1 : \n Faire fondre le chocolat \n"
+                + "Etape 2 : \n Ajouter le beurre en morceaux peu à peu et l'incorporer pour obtenir un crème bien lisse. \n "
+                + "Etape 3 : \n Mettre la farine et le sucre glace dans un saladier. Ajouter les oeufs entiers et mélanger jusqu'à obtention d'une émulsion homogène. \n "
+                + "Etape 4 : \n Verser le mélange chocolat-beurre sur cette préparation. Mélanger. \n "
+                + "Etape 5 : \n Beurrer et fariner 6 moules individuels ou un grand moule. \n "
+                + "Etape 6 : \n Verser la préparation et mettre à four chaud (200°C) - 10 min pour les petits gâteaux, 15 pour le grand. \n "
+                + "Etape 7 : \n Servir tiède avec une crème anglaise (le centre doit être coulant) ou servir froid nappé de ganache au chocolat."
+                );
+		Recette r2 = new Recette("Crepes",1,new String[]{"Sucre"},"Etape 1 : \n Dans un saladier, mélanger la farine, le sel et le sucre. \n "
+                + "Etape 2 : \n Faire une Fontaine \n "
+                + "Etape 3 : \n Ajouter les oeufs et commencer à les incorporer à la farine avec une cuillère en bois, ajouter le beure fondu (tiédi) peu à peu et bien malaxer.\n"
+                + "Etape 4 : \n Incorporer le lait (par petites quantités au début pour éviter les grumeaux), ajouter le parfum. \n "
+                + "Etape 5 : \n Faire cuire les crêpes dans une poêle chaude huilée."
+                );
+		Recette r3 = new Recette("Quiche",1,new String[]{"Sale"},
+                "Etape 1 \n Prechauffer le four, etaler la pate dans le moule \n "
+                + "Etape 2 \n la piquer avec une fourchette. Parsemer de copeaux de buerre \n "
+                + "Etape 3 : \n Faire rissoler les lardons à la poele \n "
+                + "Etape 4 : \n Battre les oeufs, la creme fraiche et le lait \n"
+                + "Etape 5 : \n Ajouter les lardons \n "
+                + "Etape 6 : \n Assaisonner de sel, poivre et de muscade \n "
+                + "Etape 7 : \n verser sur la pate \n "
+                + "Etape 8 : \n Cuire 45 à 50min \n "
+                );
+		ArrayList<Recette> r = new ArrayList<Recette>(Arrays.asList(r1,r2));
+		for (int i=0; i<r.size(); i++) {
+			m.ajouterRecette(r.get(i));
+		}
 		m.saveRecettes();
 		
 	}
