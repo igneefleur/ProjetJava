f9289d888cee279354fe26a8842e46cf09bc7c86
Edgar Poujol <epoujo01@pcalb-mm0000.univ-jfc.fr>
2021-04-14 11:20:31 +0200

    Amelioration de la checkbox

--- src/TestNote.java
+++ src/TestNote.java
@@ -1,31 +1,81 @@
-import java.awt.Checkbox;
+import java.awt.Checkbox; 
 import java.awt.CheckboxGroup;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Polygon;
+
+import javax.swing.JCheckBox;
+import javax.swing.Icon;
 
 public class TestNote {
 
 public static void main(String[] args) {
     Frame frame =new Frame();
+    frame.setLayout(new GridLayout(1,5));
+    Icon icone = new Icon() {
+
+		@Override
+		public int getIconHeight() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int getIconWidth() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public void paintIcon(Component c, Graphics g, int x, int y) {
+			// TODO Auto-generated method stub
+			g.setColor(Color.red);
+		    g.translate(x, y);
+		    Polygon poly = new Polygon();
+		    poly.addPoint(5,37);
+		    poly.addPoint(0, 35);
+		    
+		    // interieur
+		    poly.addPoint(7,31);
+		    poly.addPoint(20, 21);
+		    poly.addPoint(22, 14);
+		    poly.addPoint(21,13);
+		    poly.addPoint(19, 14);
+		    poly.addPoint(15, 21);
+		    poly.addPoint(7,31);
+		    
+		    //exterieur
+		    poly.addPoint(0,35);
+		    poly.addPoint(10, 20);
+		    poly.addPoint(15, 13);
+		    poly.addPoint(25, 10);
+		    poly.addPoint(33, 15);
+		    poly.addPoint(32,23);
+		    g.fillPolygon(poly);
+		    g.translate(-x, -y);
+		  }
+		
+    	
+    };
     
     
-    Checkbox[] etoiles = new Checkbox[5];
+    
+    JCheckBox[] etoiles = new JCheckBox[5];
     CheckboxGroup cbg = new CheckboxGroup();
     
-    String[] descrip = { "1 etoiles", "2 etoiles","3 etoiles","4 etoiles","5 etoiles"};
+    String[] descrip = { "", "","","",""};
     
     for (int i = 0; i<5; i++) {
     
-    	Checkbox cb = new Checkbox(descrip, cbg, false);
-    
+    	JCheckBox cb = new JCheckBox(descrip[i], false);
+    	
+    	cb.setIcon(icone);
     	
     	
-    	
-    	
-    	
-    	
-    	
-    	
-    	cb.setBounds(100,50, 50,50); 
+    	cb.setBounds(100,50*i,50,50); 
     	
     	frame.add(cb);
         
